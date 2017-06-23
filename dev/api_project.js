f84e92ecfb42580d1cf1b70efd2a22f29ca993ba
goodjob team (via CI) <findyourgoodjob@gmail.com>
2017-06-23 03:39:13 +0000

    apidoc for c4623b25c10c546b41d3c57d0ee75aa5f7770ebe

---
+++ dev/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "GoodJob API",
+  "version": "1.0.0",
+  "description": "GoodJob",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-03-22T16:11:35.403Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
---
+++ master/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "GoodJob API",
+  "version": "1.0.0",
+  "description": "GoodJob",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-04-20T16:25:20.721Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
